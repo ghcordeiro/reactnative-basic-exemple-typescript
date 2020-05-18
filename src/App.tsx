30f3a06c74eea313187eddaafb1f5f01ef099b19
Guilherme Cordeiro <guilherme.helly@gmail.com>
2020-05-18 10:27:25 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { View, StatusBar } from 'react-native';
+
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="dark-content"/>
+    <View />
+  </>
+)
+
+export default App;
